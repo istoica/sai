98953232084302f64802eb8f0f5aa3f5764f2bcb
Ion Stoica <istoica@ion-YR27Q67T64.local>
2026-02-14 22:04:22 -0800

    Remove the target directory

--- target/debug/deps/sorter-f81b2914c5c3bd1d.d
+++
@@ -1,5 +0,0 @@
-/Users/istoica/src/26/sai/target/debug/deps/sorter-f81b2914c5c3bd1d.d: src/main.rs
-
-/Users/istoica/src/26/sai/target/debug/deps/sorter-f81b2914c5c3bd1d: src/main.rs
-
-src/main.rs:
--- target/debug/sorter.d
+++
@@ -1,1 +0,0 @@
-/Users/istoica/src/26/sai/target/debug/sorter: /Users/istoica/src/26/sai/src/main.rs
